b9c615bb90a1c92397d1880b8a7e3d81a57909eb
tmohammad78 <moh.taheri@alibaba.ir>
2025-06-22 21:05:45 +0330

    feat: add docker and delete and update

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductRequest(Guid id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record UpdateProductResponse(bool IsSuccess);
+internal class UpdateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpdateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<UpdateProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("UpdateProduct")
+        .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Update Product")
+        .WithDescription("Update Product");
+    }
+}
